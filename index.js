444ac552f69dbeb7763230cfbbbfe4882eb163e0
gkoe <gkoe@gmx.at>
2018-10-08 14:49:27 +0200

    resolves #4

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- @Furtmueller Sign out
-1
-0 1 rstropek/GitHubEduWorkshop
- Code  Issues 2  Pull requests 0  Projects 0  Wiki  Insights
-GitHubEduWorkshop/hands-on-labs/github-flow/step-2/index.js
-a086844  23 hours ago
-@rstropek rstropek Add hands-on lab
-     
-26 lines (21 sloc)  851 Bytes
 const parameter1Input = document.getElementById('parameter1');
 const parameter2Input = document.getElementById('parameter2');
 const operationSelect = document.getElementById('operation');
@@ -34,22 +17,27 @@ function calculate() {
         errorArea.hidden = true;
     }
 
-    // ToDo: Call calculation logic. For now, we just assume the universal answer to everything ;-)
-    let result = 42;
+    let result;
+    switch (operationSelect.value) {
+        case 'add':
+            result = add(parseInt(parameter1Input.value), parseInt(parameter2.value));
+            break;
+        case 'div':
+            result = div(parseInt(parameter1Input.value), parseInt(parameter2.value));
+            break;
+        default:
+            console.error('Invalid operation.');
+            return;
+    }
 
     resultSpan.innerText = result.toString();
     resultArea.hidden = false;
 }
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
+
+function add(x, y) {
+    return x + y;
+}
+
+function div(x, y) {
+    return x / y;
+}
